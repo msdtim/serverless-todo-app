aa303282cb7eace16525f0c7cc714ac7faeb0cd4
Shuda Mo <msdtim@gmail.com>
2020-04-19 01:14:51 +0800

    feat: Impletement Auth0
    Authentication is done using JWKS url
    https://auth0.com/blog/navigating-rs256-and-jwks/
    Reuse some of the js code from git
    https://github.com/sgmeyer/auth0-node-jwks-rs256/blob/df2f15c7c07db72b239d3beb13ad0f089b224791/src/lib/JwksClient.js#L11

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-4j7w7l3d.auth0.com',            // Auth0 domain
+  clientId: 'Qwf9AbdbvL6rqW2AY3p5Aa4vXu7BK3Lk',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
