0d43665ca168bd7018f36cf29fe501ca0f439184
Shuda Mo <msdtim@gmail.com>
2020-04-18 19:31:16 +0800

    feat: updateTodo
    Noted the "name" attr in item table conflicts with dynamoDB reserve words, use # then ExpressionAttributeNames to get around it

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -18,6 +18,10 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
     body: JSON.stringify({
         items: todos,
     }),
